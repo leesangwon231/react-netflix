76f81dcbd068e318642dc7485d88a1cbce50f496
leesangwon <werggf@naver.com>
2024-08-27 14:05:25 +0900

    noona react netflix 2st commit add background

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <img className={"img"} src={"https://assets.nflxext.com/ffe/siteui/vlv3/36a4db5b-dec2-458a-a1c0-662fa60e7473/0e19e43e-655e-4150-b74a-e0028dd78b6a/KR-ko-20240820-TRIFECTA-perspective_WEB_ac15e26c-11f9-4a9a-b934-5b7089743290_large.jpg"}></img>
+        <div className={"background"}></div>
     </div>
   );
 }
